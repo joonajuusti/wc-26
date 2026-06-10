ec8f1764ced9b5d253355ff875094f6e4ede4cc9
Joona Juusti <joona.juusti@gmail.com>
2026-06-10 11:36:08 +0300

    Redirect user to app from login page if logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { getSessionUser } from "@/lib/auth";
 import { LoginForm } from "./login-form";
+import { redirect, RedirectType } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function LoginPage() {
+  const user = await getSessionUser();
+
+  if (!!user) {
+    redirect("/predictions", RedirectType.replace);
+  }
+
   return (
     <div className="flex min-w-0 flex-1 items-center justify-center px-4">
       <div className="w-full max-w-sm">
