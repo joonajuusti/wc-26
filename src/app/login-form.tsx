db2ec2c675c3bf3b6f51dd143e791445513ddfb6
Joona Juusti <joona.juusti@gmail.com>
2026-06-10 18:54:54 +0300

    Change login input font size to avoid auto-zoom on iOS

--- src/app/login-form.tsx
+++ src/app/login-form.tsx
@@ -39,7 +39,7 @@ export function LoginForm() {
           id="inviteCode"
           name="inviteCode"
           required
-          className="mt-1 block w-full rounded-md border border-zinc-300 bg-white px-3 py-2 text-sm shadow-sm placeholder:text-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 "
+          className="mt-1 block w-full rounded-md border border-zinc-300 bg-white px-3 py-2 text-base shadow-sm placeholder:text-zinc-400 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500 "
         />
       </div>
 
